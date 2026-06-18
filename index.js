a474b3e97e35486b854c9edbd785ba1bd3e70b83
Yuri Arthur <yuarthuraalves@gmail.com>
2026-06-18 12:16:32 -0300

    Clean up blank lines in index.js
    Removed unnecessary blank lines from index.js

--- index.js
+++ index.js
@@ -253,34 +253,6 @@ app.get('/usuarios/remover/:id', async (req, res) => {
     }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
